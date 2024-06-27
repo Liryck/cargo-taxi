829258c4d3a919c47abc7c1e462d5a1b5b181dc3
Kyrylo Brahin <34404333+Liryck@users.noreply.github.com>
2024-06-28 00:11:05 +0300

    Update smart.php

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'sr50.hostlife.net';  // Specify main and backup SMTP servers
+$mail->Host = '';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'order@vantag-taxi.kyiv.ua';                 // Наш логин
-$mail->Password = 'Gu$X;p@N7Lx8';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('order@vantag-taxi.kyiv.ua', 'Vantag Taxi');   // От кого письмо 
-$mail->addAddress('vantagtaxi@ukr.net');     // Add a recipient
+$mail->setFrom('', '');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
